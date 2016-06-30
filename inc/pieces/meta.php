621612c36455ed09d5ee30c0a05cb6dbd4466f4a
Simon Bédard <simon@cyclemapapp.com>
2016-06-30 18:20:32 -0400

    Création de la meta

---
+++ inc/pieces/meta.php
@@ -0,0 +1,9 @@
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<title>Simon Bédard </title>
+	<link rel="stylesheet" type="text/css" href="<?php echo $strchemin ?>css/main.css">
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+	<!-- <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'> -->
+</head>
